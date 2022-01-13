28120b48b0c3a47910d3e437a3d6bf12b6a431f7
Daniel Schischkin <daniel@schischkin.info>
2022-01-13 03:44:45 +0100

    Remove unnecessary bucket

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import * as bucket from "./furni_data/bucket"
 import * as pubsub from "./furni_data/pubsub"
 import * as functions from "./furni_data/functions"
 
-export { bucket, pubsub, functions }
+export { pubsub, functions }
